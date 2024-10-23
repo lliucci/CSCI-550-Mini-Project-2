88b048f3c2ec5b47f86a97822b2dc9b71dbf9ffb
Eliot Liucci <eliotliucci123@gmail.com>
2024-10-23 09:38:11 -0600

    Updated Data Cleaning

--- Data_Cleaning.R
+++ Data_Cleaning.R
@@ -15,7 +15,7 @@ rooms = as.numeric(0)
 bedrooms = as.numeric(0)
 baths = as.numeric(0)
 
-  # Takes a minute to run, not too bad though
+# Takes a minute to run, not too bad though
 for(i in 1:nrow(data)){
     sell_date[i] = str_split(str_split(data$Description[i], "sold on ")[[1]][2], ", is a")[[1]][1]
     rooms[i] = str_extract_all(str_split(data$Description[i], "total of ")[[1]][2], "\\d")[[1]][1]
@@ -37,8 +37,6 @@ data = data %>%
 data = data %>%
     filter(Bedrooms < Rooms | Baths < Rooms)
 
-
-## Other modifications
 
 # Removing rows with at least 1 missing value
 data = data %>%
@@ -64,88 +62,46 @@ data = data %>%
            !is_outlier(`Building Square Feet`),
            !is_outlier(`Other Improvements`))
 
+# Removing Spaces
+data = data %>%
+  rename(Property_Class = `Property Class`,
+        Neighborhood_Code = `Neighborhood Code`,
+        Land_Square_Feet = `Land Square Feet`,
+        Town_Code = `Town Code`,
+        Wall_Material = `Wall Material`,
+        Roof_Material = `Roof Material`,
+        Basement_Finish = `Basement Finish`,
+        Central_Heating = `Central Heating`,
+        Other_Heating = `Other Heating`,
+        Central_Air = `Central Air`,
+        Attic_Type = `Attic Type`,
+        Attic_Finish = `Attic Finish`,
+        Design_Plan = `Design Plan`,
+        Cathedral_Ceiling = `Cathedral Ceiling`,
+        Garage_1_Size = `Garage 1 Size`,
+        Garage_1_Material = `Garage 1 Material`,
+        Garage_1_Attachment = `Garage 1 Attachment`,
+        Garage_1_Area = `Garage 1 Area`,
+        Garage_2_Size = `Garage 2 Size`,
+        Garage_2_Material = `Garage 2 Material`,
+        Garage_2_Attachment = `Garage 2 Attachment`,
+        Garage_2_Area = `Garage 2 Area`,
+        Building_Square_Feet = `Building Square Feet`,
+        Multi_Code = `Multi Code`,
+        Number_of_Commercial_Units = `Number of Commercial Units`,
+        Estimate_Land = `Estimate (Land)`,
+        Estimate_Building = `Estimate (Building)`,
+        Sale_Price = `Sale Price`,
+        Multi_Property_Indicator = `Multi Property Indicator`,
+        OHare_Noise = `O'Hare Noise`,
+        Road_Proximity = `Road Proximity`,
+        Sale_Year = `Sale Year`,
+        Sale_Quarter_of_Year = `Sale Quarter of Year`,
+        Sale_Month_of_Year = `Sale Month of Year`,
+        Most_Recent_Sale = `Most Recent Sale`,
+        Pure_Market_Filter = `Pure Market Filter`,
+        Garage_Indicator = `Garage Indicator`,
+        Lot_Size = `Lot Size`)
 
-# Removing columns that are not needed
-# Label "NA" meaning codebook said not to include it
-data <- data %>% 
-  select(-c(
-    `...1`, # Just the row number
-    PIN, # Identifier for row, not necessary for regression
-    `Construction Quality`, # NA
-    `Site Desirability`, # NA
-    `Other Improvements`, # NA
-    `Repair Condition`, ## NA maybe, Idk if we should keep it
-    `Deed No.`, # NA
-    Longitude, # NA
-    Latitude, # NA
-    `Census Tract`, ## NA, maybe. Could be used for plotting/clustering
-    `Modeling Group`, # NA
-    Use, # NA
-    `Sale Quarter`, # This variable doesn't make sense to me. `Sale Quarter of Year` makes more sense to keep
-    `Sale Half-Year`, # This variable doesn't make sense to me. `Sale Half of Year` makes more sense to keep
-    `Sale Half of Year`, # Variable information captured in `Sale Quarter of Year`
-    `Neigborhood Code (mapping)`, ## NA, maybe. Could be used for plotting/clustering
-    `Town and Neighborhood`, ## NA unless we treat it as a random variable, it has 800 levels
-    `Age Decade` # This variable is better captured in `Age`
-  ))
-
-# Mutating variables to make sure they are of the right type
-# Also scaling occurs in here
-data <- data %>% 
-  mutate(
-    # `Repair Condition` = as.factor(`Repair Condition`),
-    # `Census Tract` = as.factor(`Census Tract`),
-    # `Neighborhood Code (mapping)` = as.factor(`Neigborhood Code (mapping)`),
-    # `Town and Neighborhood` = as.factor(`Town and Neighborhood`),
-    `Property Class` = as.factor(`Property Class`),
-    `Land Square Feet` = as.numeric(`Land Square Feet`),
-    `Town Code` = as.factor(`Town Code`),
-    Apartments = as.numeric(Apartments),
-    `Wall Material` = as.factor(`Wall Material`),
-    `Roof Material` = as.factor(`Roof Material`),
-    Basement = as.factor(Basement),
-    `Basement Finish` = as.factor(`Basement Finish`),
-    `Central Heating` = as.factor(`Central Heating`),
-    `Other Heating` = as.factor(`Other Heating`),
-    `Central Air` = as.factor(`Central Air`),
-    Fireplaces = as.factor(Fireplaces),
-    `Attic Type` = as.factor(`Attic Type`),
-    `Attic Finish` = as.factor(`Attic Finish`),
-    `Design Plan` = as.factor(`Design Plan`),
-    `Cathedral Ceiling` = as.factor(`Cathedral Ceiling`),
-    `Garage 1 Area` = as.factor(`Garage 1 Area`),
-    `Garage 1 Size` = as.factor(`Garage 1 Size`),
-    `Garage 1 Material` = as.factor(`Garage 1 Material`),
-    `Garage 1 Attachment` = as.factor(`Garage 1 Attachment`), 
-    `Garage 2 Area` = as.factor(`Garage 2 Area`),
-    `Garage 2 Size` = as.factor(`Garage 2 Size`),
-    `Garage 2 Material` = as.factor(`Garage 2 Material`),
-    `Garage 2 Attachment` = as.factor(`Garage 2 Attachment`),
-    Porch = as.factor(Porch),
-    `Building Square Feet` = as.numeric(`Building Square Feet`),
-    `Multi Code` = as.factor(`Multi Code`),
-    `Number of Commercial Units` = as.numeric(`Number of Commercial Units`),
-    `Estimate (Land)` = as.numeric(`Estimate (Land)`),
-    `Estimate (Building)` = as.numeric(`Estimate (Building)`),
-    `Sale Price` = as.numeric(`Sale Price`),
-    # Add a scale for Sale Price here?
-    `Multi Property Indicator` = as.factor(`Multi Property Indicator`),
-    Age = as.numeric(Age),
-    `O'Hare Noise` = as.factor(`O'Hare Noise`),
-    Floodplain = as.factor(Floodplain),
-    `Road Proximity` = as.factor(`Road Proximity`),
-    `Sale Year` = as.numeric(`Sale Year`),
-    `Sale Year` = `Sale Year` - 2013, # Scaling Sale Year to start at 0
-    `Sale Quarter of Year` = as.factor(`Sale Quarter of Year`),
-    `Sale Month of Year` = as.factor(`Sale Month of Year`),
-    `Most Recent Sale` = as.factor(`Most Recent Sale`),
-    `Pure Market Filter` = as.factor(`Pure Market Filter`),
-    `Garage Indicator` = as.factor(`Garage Indicator`),
-    `Neigborhood Code` = as.factor(`Neighborhood Code`),
-    `Lot Size` = as.numeric(`Lot Size`),
-  )
-
-# Print out the summary statistics for each variable
-#apply(data, 2, favstats)
-
+# Save cleaned data
 write_csv(data, "data_cleaned.csv")
